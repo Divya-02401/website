7a5d26244d79cfe37bf2e43938464da7e1946380
Divya <divyagudipati.g@gmail.com>
2023-06-21 17:57:49 +0530

    added js

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 let b=prompt("Hey, Whats the role you are applying for?")
 alert("Well.That's Great.All the best")
+
+//console.assert("3>12")
+//console.error("this is error")
+//console.time("forloop")
+//for(let i=0;i<5;i++){
+//	console.log(2)
+//}
+//console.timeEnd("forloop")
+//let i=0;
+//while(i<5){
+//	console.log(2)}
+//console.timeEnd(whileloop)
+//alert("enter the value of a:")
+//let a=prompt("enter a")
+//a=Number.parseInt(a)
+//alert("you entered the type of " +(typeof a))
+//let write=confirm("do you want to write it to the page")
+//if(write){
+//	document.write(a)
+//}
+//else{
+//	document.write("please allow mw to write")
+//}
+//document.write(a)
+//console.log(document.body)
+document.body.style.background = "violet"
+//console.log(document.body.firstChild)
