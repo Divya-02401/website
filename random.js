250ce7dee44dceb46e4b6ad5630bafdf92101382
Divya <divyagudipati.g@gmail.com>
2023-07-08 17:21:12 +0530

    updated

---
+++ random.js
@@ -0,0 +1,30 @@
+
+/*let btn=document.getElementById('btn');
+let output=document.getElementById('outputText');
+let clk=document.getElementById('total-count');
+
+let randNum=Math.floor(Math.random()*100)
+btn.addEventListener('click',function(){
+let input=document.getElementById("userInput").value;
+if(input==randNum){
+	output.innerHTML="your guess is right" 
+}
+else
+if(input>randNum){
+	output.innerHTML="your guess is high, TRY AGAIN"
+}
+else
+if(input<randNum){
+	output.innerHTML="your guess is low, TRY AGAIN"
+}
+},function(){
+	let count=1;
+	let limit=10
+	if(count<limit){
+	clk.innerHTML=count;
+	count=count+1;
+}
+}
+);
+*/
+
